d1a615e72ef002a7a6251dc692cb8c50e250f186
egor <pojymar@gmail.com>
2022-07-27 15:28:24 +0300

    initial commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from typing import List
 
+import uvicorn
 from fastapi import FastAPI, Depends
 from fastapi.openapi.models import APIKey
 from starlette.responses import JSONResponse
@@ -8,7 +9,7 @@ from model import PydanticAction
 from repo import ActionAlchemyRepository, get_action_repo
 from utils.auth import get_api_key
 
-app = FastAPI()
+app = FastAPI(swagger_ui_parameters={"tryItOutEnabled":True})
 
 
 @app.exception_handler(Exception)
@@ -23,10 +24,12 @@ def action_create(action: PydanticAction, api_key: APIKey = Depends(get_api_key)
     pass
 
 
-@app.get("/actions/{_id}",  response_model=List[PydanticAction])
+@app.get("/actions/{_id}")
 def action_fetch_by_id(_id: int, api_key: APIKey = Depends(get_api_key),
                        repo: ActionAlchemyRepository = Depends(get_action_repo)) -> PydanticAction:
     """Возвращает Action со всеми потомками и всей информацией о нём (теги, заметки)
+    Запрос:
+        action_id = 1
     Ответ:
         {
           "action_id": 1,
@@ -72,11 +75,11 @@ def action_fetch_by_id(_id: int, api_key: APIKey = Depends(get_api_key),
           ]
         }
     """
-    action = repo.fetch_by_id(_id)
+    action = repo.fetch_by_action_id(_id)
     return action
 
 
-@app.put('/actions/by_name/{name}')
+@app.get('/actions/by_name/{name}')
 def action_fetch_by_action_name(action_name: str, api_key: APIKey = Depends(get_api_key),
                                 repo: ActionAlchemyRepository = Depends(get_action_repo)) -> List[PydanticAction]:
     """Ищет похожие по Action.action (SQL LIKE). Возвращает список Action c sub-Actions первого уровня вложенности
@@ -112,7 +115,8 @@ def action_fetch_by_action_name(action_name: str, api_key: APIKey = Depends(get_
           }
         ]
         """
-    pass
+    action = repo.fetch_by_action_name(action_name)
+    return action
 
 
 @app.put('/actions/{action_id}')
@@ -121,4 +125,5 @@ def action_update(action: PydanticAction, api_key: APIKey = Depends(get_api_key)
     pass
 
 
-
+if __name__ == '__main__':
+    uvicorn.run("__main__:app", host="127.0.0.1", port=8220, reload=True)
