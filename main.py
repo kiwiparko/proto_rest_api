b51ecf3d3f9eea7a06734be90d7495135fb603ce
egor <pojymar@gmail.com>
2022-07-30 20:13:32 +0300

    add 'action_create' and 'action_update' funcs. fix 'fetch_by_action_id' func.

--- main.py
+++ main.py
@@ -21,60 +21,14 @@ def validation_exception_handler(request, err):
 @app.post("/actions/")
 def action_create(action: PydanticAction, api_key: APIKey = Depends(get_api_key),
                   repo: ActionAlchemyRepository = Depends(get_action_repo)):
-    pass
+    print(action)
+    repo.create_pydantic(action, return_pydantic=True)
+    return action
 
 
 @app.get("/actions/{_id}")
 def action_fetch_by_id(_id: int, api_key: APIKey = Depends(get_api_key),
                        repo: ActionAlchemyRepository = Depends(get_action_repo)) -> PydanticAction:
-    """Возвращает Action со всеми потомками и всей информацией о нём (теги, заметки)
-    Запрос:
-        action_id = 1
-    Ответ:
-        {
-          "action_id": 1,
-          "action": "My first Action",
-          "children": [
-            {
-              "action_id": 2,
-              "action": "sub Action 1",
-              "children": [
-                {
-                  "action_id": 3,
-                  "action": "sub sub Action 1",
-                  "group": "SOON",
-                  "tags": [{"name": "work", "color": "#b0c4de"}],
-                  "created_on": "2022-07-26T10:24:49.959Z",
-                  "updated_on": "2022-07-26T10:24:49.959Z",
-                  "notes": [
-                    {
-                      "action_id": 3,
-                      "type": "image",
-                      "payload": "BASE64=="
-                    }
-                  ]
-                }
-              ],
-              "group": "SOON",
-              "tags": [{"name": "work", "color": "#b0c4de"}],
-              "created_on": "2022-07-26T10:24:49.959Z",
-              "updated_on": "2022-07-26T10:24:49.959Z",
-              "notes": [ ]
-            }
-          ],
-          "group": "SOON",
-          "tags": [{"name": "work", "color": "#b0c4de"}],
-          "created_on": "2022-07-26T10:24:49.959Z",
-          "updated_on": "2022-07-26T10:24:49.959Z",
-          "notes": [
-            {
-              "action_id": 1,
-              "type": "image",
-              "payload": "BASE64=="
-            }
-          ]
-        }
-    """
     action = repo.fetch_by_action_id(_id)
     return action
 
@@ -82,47 +36,16 @@ def action_fetch_by_id(_id: int, api_key: APIKey = Depends(get_api_key),
 @app.get('/actions/by_name/{name}')
 def action_fetch_by_action_name(action_name: str, api_key: APIKey = Depends(get_api_key),
                                 repo: ActionAlchemyRepository = Depends(get_action_repo)) -> List[PydanticAction]:
-    """Ищет похожие по Action.action (SQL LIKE). Возвращает список Action c sub-Actions первого уровня вложенности
-    c limit 20.
-     Пример:
-        Запрос:
-          action_name = 'My first'
-        Ответ:
-        [
-          {
-            "action_id": 1,
-            "action": "My first Action",
-            "children": [
-              {
-                "action_id": 2,
-                "action": "sub Action 1",
-                "group": "SOON",
-                "created_on": "2022-07-26T10:24:49.959Z",
-                "updated_on": "2022-07-26T10:24:49.959Z"
-              },
-              {
-                "action_id": 3,
-                "action": "sub Action 2",
-                "group": "SOON",
-                "created_on": "2022-07-26T10:24:49.959Z",
-                "updated_on": "2022-07-26T10:24:49.959Z"
-              }
-            ],
-            "group": "SOON",
-            "tags": [{"name": "work", "color": "#b0c4de"}],
-            "created_on": "2022-07-26T10:24:49.959Z",
-            "updated_on": "2022-07-26T10:24:49.959Z"
-          }
-        ]
-        """
     action = repo.fetch_by_action_name(action_name)
     return action
 
 
 @app.put('/actions/{action_id}')
-def action_update(action: PydanticAction, api_key: APIKey = Depends(get_api_key),
+def action_update(action_id: int, action: PydanticAction, api_key: APIKey = Depends(get_api_key),
                   repo: ActionAlchemyRepository = Depends(get_action_repo)):
-    pass
+    print(action)
+    result = repo.update_pydantic(action, action_id)
+    return result
 
 
 if __name__ == '__main__':
